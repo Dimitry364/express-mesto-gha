f36daed1fa3a6aa629f509824c03abe3783ce54c
Dimitry364 <g4779@yandex.ru>
2024-02-01 20:18:28 +0700

    Правки ревью

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const { login, createUser } = require("./controllers/users");
 const auth = require("./middlewares/auth");
 const errorHandler = require("./middlewares/errorHandler");
 const { loginValidator, userValidator } = require("./middlewares/validation");
-const mongodb = require("./utils/mongodb");
+const { mongodb } = require("./utils/mongodb");
 
 const { PORT = 3000, DB_CONN = mongodb } = process.env;
 const app = express();
